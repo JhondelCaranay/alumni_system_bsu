e309ad8d98f201b10defb51c98ce49893af1c360
jhondel <jhondeldelconacaranay@gmail.com>
2023-10-21 03:29:17 +0800

    seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Role } from "@prisma/client";
+import { Gender, PrismaClient, Role } from "@prisma/client";
 import bcrypt from "bcrypt";
 import { faker } from "@faker-js/faker";
 /* 
@@ -27,6 +27,7 @@ async function main() {
   const password = await bcrypt.hash("dev123", 12);
 
   const users_roles = [
+    "ADMIN",
     "FACULTY_MEMBER",
     "FACULTY_ADVISER",
     "FACULTY_COORDINATOR",
@@ -36,29 +37,90 @@ async function main() {
     "BULSU_PARTNER",
   ];
 
-  await prisma.user.create({
-    data: {
-      email: "admin@gmail.com",
-      hashedPassword: password,
-      role: Role.ADMIN,
-    },
+  users_roles.forEach(async (role) => {
+    const user = await prisma.user.create({
+      data: {
+        email: `${role.toLowerCase()}@bulsu.edu.ph`,
+        hashedPassword: password,
+        role: Role[role as keyof typeof Role],
+        name: faker.person.firstName(),
+        image: faker.image.avatar(),
+      },
+    });
+
+    const isAlumniOrStudent =
+      user.role === Role.FACULTY_AlUMNI || user.role === Role.FACULTY_STUDENT;
+
+    // profile
+    await prisma.profile.create({
+      data: {
+        studentNumber: isAlumniOrStudent ? faker.number.int().toString() : undefined,
+        yearEnrolled: isAlumniOrStudent
+          ? faker.date.between({ from: "2020-01-01T00:00:00.000Z", to: "2030-01-01T00:00:00.000Z" })
+          : undefined,
+        alternative_email: faker.internet.email(),
+        firstname: faker.person.firstName(),
+        lastname: faker.person.lastName(),
+        middlename: faker.person.lastName(),
+        age: faker.number.int({ min: 18, max: 60 }),
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
   });
 
-  //   const alice = await prisma.user.upsert({
-  //     where: { email: "alice@prisma.io" },
-  //     update: {},
-  //     create: {
-  //       email: "alice@prisma.io",
-  //       name: "Alice",
-  //       posts: {
-  //         create: {
-  //           title: "Check out Prisma with Next.js",
-  //           content: "https://www.prisma.io/nextjs",
-  //           published: true,
-  //         },
-  //       },
-  //     },
-  //   });
+  // create 10 alumni and 10 students
+  for (let i = 0; i < 20; i++) {
+    const isAlumniOrStudent = i % 2 === 0;
+    // alumni is even, student is odd
+
+    const user = await prisma.user.create({
+      data: {
+        email: `
+        ${faker.person.firstName()}@bulsu.edu.ph
+        `,
+        hashedPassword: password,
+        role: isAlumniOrStudent ? Role.FACULTY_AlUMNI : Role.FACULTY_STUDENT,
+        name: faker.person.firstName(),
+        image: faker.image.avatar(),
+      },
+    });
+
+    // profile
+    await prisma.profile.create({
+      data: {
+        studentNumber: faker.number.int().toString(),
+        yearEnrolled: faker.date.between({
+          from: "2020-01-01T00:00:00.000Z",
+          to: "2030-01-01T00:00:00.000Z",
+        }),
+        alternative_email: faker.internet.email(),
+        firstname: faker.person.firstName(),
+        lastname: faker.person.lastName(),
+        middlename: faker.person.lastName(),
+        age: faker.number.int({ min: 18, max: 60 }),
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+  }
+
+  // course / department / program
+  const departments = ["AUTOMOTIVE", "COMPUTER", "DRAFTING", "EIR", "EEC", "FOODS", "MECHANICAL"];
+
+  departments.forEach(async (department) => {
+    await prisma.department.create({
+      data: {
+        name: department,
+      },
+    });
+  });
 
   console.log("Seeding completed.");
 }

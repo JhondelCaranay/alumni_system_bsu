b450a34e6e426d69276443bcfc8b3057bdf5a53b
jhondel <jhondeldelconacaranay@gmail.com>
2023-10-24 19:35:35 +0800

    department crud api

--- src/queries/students.ts
+++ src/queries/students.ts
@@ -1,10 +1,34 @@
-import { SafeUserWithProfileWithDapartment } from "@/types/types";
+import { SafeUserWithProfileWithDapartmentWithSection } from "@/types/types";
 import axios from "axios";
 
-export const getStudents = (queryString?: string): Promise<SafeUserWithProfileWithDapartment[]> => {
-  if (!queryString) {
+import q from "query-string";
+
+export type getStudentsQuery = {
+  role?: string;
+  schoolYear?: string;
+  department?: string;
+};
+
+export const getStudents = ({
+  role,
+  schoolYear,
+  department,
+}: getStudentsQuery): Promise<SafeUserWithProfileWithDapartmentWithSection[]> => {
+  let query = q.stringify(
+    {
+      role,
+      schoolYear,
+      department,
+    },
+    {
+      skipEmptyString: true,
+      skipNull: true,
+    }
+  );
+
+  if (!query) {
     return axios.get("/api/students").then((response) => response.data);
   } else {
-    return axios.get(`/api/students?${queryString}`).then((response) => response.data);
+    return axios.get(`/api/students?${query}`).then((response) => response.data);
   }
 };

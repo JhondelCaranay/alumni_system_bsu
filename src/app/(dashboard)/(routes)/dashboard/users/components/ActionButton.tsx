3b8808f62ba42023d89d36e315ed30274a71650d
AndroPyro28 <menandroeugenio1028@gmail.com>
2023-10-26 15:03:07 +0800

    Events crud and design done

--- src/app/(dashboard)/(routes)/dashboard/users/components/ActionButton.tsx
+++ src/app/(dashboard)/(routes)/dashboard/users/components/ActionButton.tsx
@@ -2,7 +2,7 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import { useModal } from '@/hooks/useModalStore';
 import { SafeDeparment, SafeUser, UserProfileWithDepartmentSection, UserWithProfile } from '@/types/types';
 import { User } from '@prisma/client';
-import { MoreHorizontal } from 'lucide-react';
+import { Archive, MoreHorizontal, Pencil } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React from 'react'
 
@@ -21,18 +21,20 @@ const ActionButton:React.FC<ActionButtonProps> = ({user}) => {
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
               <DropdownMenuItem className="text-xs cursor-pointer hover:bg-zinc-400" onClick={() => router.push(`users/${user.id}`)}>
-                Update
+<Pencil className="h-4 w-4 mr-2" />
+                 Update
               </DropdownMenuItem>
               <DropdownMenuItem
-                className="text-xs cursor-pointer hover:bg-zinc-400"
+                className="text-xs cursor-pointer text-red-600 hover:!text-red-600 hover:!bg-red-100"
                 onClick={() => onOpen("archiveUser", { user: user })}
+
               >
-                Archive
+<Archive className="h-4 w-4 mr-2" />
+                 Archive
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
   )
 }
-
 export default ActionButton

a33c955b495d0019392a84e75d44f0de105ac581
AndroPyro28 <menandroeugenio1028@gmail.com>
2023-11-02 22:20:50 +0800

    Froala Editor

---
+++ src/app/(dashboard)/(routes)/dashboard/jobs/create/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import Editor from '../components/Editor'
+
+const page = () => {
+  return (
+    <div className='p-5'>
+      <Editor />
+    </div>
+  )
+}
+
+export default page

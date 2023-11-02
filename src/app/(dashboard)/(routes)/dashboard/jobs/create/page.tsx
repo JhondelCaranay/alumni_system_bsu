ca7f9c447ba5f3f8b7c19d42bc2d8fb5b6b3b2df
jhondel <jhondeldelconacaranay@gmail.com>
2023-11-03 01:31:54 +0800

    update text editor

--- src/app/(dashboard)/(routes)/dashboard/jobs/create/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/jobs/create/page.tsx
@@ -1,13 +1,17 @@
-"use client"
-import React from 'react'
-import Editor from '../components/Editor'
+import React from "react";
+// import Editor from "../components/Editor";
+import dynamic from "next/dynamic";
+
+const Editor = dynamic(() => import("./../components/Editor"), {
+  ssr: false,
+});
 
 const page = () => {
   return (
-    <div className='p-5'>
+    <div className="p-5">
       <Editor />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;

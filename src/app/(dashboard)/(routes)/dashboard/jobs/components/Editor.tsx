d511dfdf8ec07ac1b6d2d38d9f9ffc14fc2ead99
jhondel <jhondeldelconacaranay@gmail.com>
2023-11-04 02:34:43 +0800

    remove insert files in text editor

--- src/app/(dashboard)/(routes)/dashboard/jobs/components/Editor.tsx
+++ src/app/(dashboard)/(routes)/dashboard/jobs/components/Editor.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import FroalaEditorComponent from "react-froala-wysiwyg";
 
@@ -47,9 +46,74 @@ const Editor: React.FC<EditorProps> = ({ model, onChange }) => {
           fontFamilySelection: true,
           fontSizeSelection: true,
           paragraphFormatSelection: true,
+
           events: {
             "save.before": function (html: string) {
               localStorage.setItem("savedContent", html);
+            },
+          },
+
+          toolbarButtons: {
+            moreText: {
+              buttons: [
+                "bold",
+                "italic",
+                "underline",
+                "strikeThrough",
+                "subscript",
+                "superscript",
+                "fontFamily",
+                "fontSize",
+                "textColor",
+                "backgroundColor",
+                "inlineClass",
+                "inlineStyle",
+                "clearFormatting",
+              ],
+            },
+            moreParagraph: {
+              buttons: [
+                "alignLeft",
+                "alignCenter",
+                "formatOLSimple",
+                "alignRight",
+                "alignJustify",
+                "formatOL",
+                "formatUL",
+                "paragraphFormat",
+                "paragraphStyle",
+                "lineHeight",
+                "outdent",
+                "indent",
+                "quote",
+              ],
+            },
+            moreRich: {
+              buttons: [
+                "insertLink",
+                // "insertImage",
+                // "insertVideo",
+                "insertTable",
+                "emoticons",
+                "fontAwesome",
+                "specialCharacters",
+                "embedly",
+                // "insertFile",
+                "insertHR",
+              ],
+            },
+            moreMisc: {
+              buttons: [
+                "undo",
+                "redo",
+                "fullscreen",
+                // "print",
+                // "getPDF",
+                "spellChecker",
+                "selectAll",
+                "html",
+                "help",
+              ],
             },
           },
         }}

cf1b18f22c6aa3ee38f4238b588bd155c6e1bb90
jhondel <jhondeldelconacaranay@gmail.com>
2023-11-21 20:10:25 +0800

    fix mobile sidebar not showing

--- src/app/(main-layout)/components/MobileSidebar.tsx
+++ src/app/(main-layout)/components/MobileSidebar.tsx
@@ -2,15 +2,21 @@ import { Menu } from "lucide-react";
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Sidebar } from "./Sidebar";
+import { SafeUser } from "@/types/types";
 
-export const MobileSidebar = () => {
+type MobileSidebarProps = {
+  currentUser?: SafeUser | null;
+};
+
+export const MobileSidebar = ({ currentUser }: MobileSidebarProps) => {
+  console.log("🚀 ~ file: MobileSidebar.tsx:12 ~ MobileSidebar ~ currentUser:", currentUser)
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-white">
-        <Sidebar />
+        <Sidebar currentUser={currentUser} />
       </SheetContent>
     </Sheet>
   );

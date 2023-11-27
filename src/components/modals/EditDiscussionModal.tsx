8394b9645c66892a430045f1dbb75be7101de9f3
AndroPyro28 <menandroeugenio1028@gmail.com>
2023-11-28 01:58:44 +0800

    edit discussion

--- src/components/modals/EditDiscussionModal.tsx
+++ src/components/modals/EditDiscussionModal.tsx
@@ -46,22 +46,21 @@ import {
 import { DepartmentSchemaType } from "@/schema/department";
 import { Checkbox } from "../ui/checkbox";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import { ChevronsUpDown, X, Image as ImageUpload} from "lucide-react";
+import { ChevronsUpDown, X, Image as ImageUpload } from "lucide-react";
 import EmojiPicker from "../EmojiPicker";
 import ActionTooltip from "../ActionTooltip";
 import { Input } from "../ui/input";
-import axios from "axios";
-import { CreatePostSchemaType, PostSchemaType } from "@/schema/post";
-import { PostType, Role } from "@prisma/client";
+import { PostSchemaType, UpdatePostSchemaType } from "@/schema/post";
 import { useToast } from "../ui/use-toast";
-import Image from "next/image";
+import { uploadPhotoForum } from "@/lib/utils";
 
 const EditDiscussionModal = () => {
   const { data: session } = useSession();
   const { isOpen, type, onClose, data } = useModal();
   const isModalOpen = isOpen && type === "editDiscussion";
-  const [filesToDisplay, setFilesToDisPlay] = useState<{ url: string; id: number | string }[]
->([]);
+  const [filesToDisplay, setFilesToDisPlay] = useState<
+    { url: string; id: string }[]
+  >([]);
 
   const onHandleClose = () => {
     onClose();
@@ -77,8 +76,8 @@ const EditDiscussionModal = () => {
       .refine((value) => value.some((item) => item), {
         message: "You have to select at least one department",
       }),
-    photosToCreate: z.any().optional(),
-    photosToDelete: z.any().optional(),
+    new_photos: z.array(z.any()).optional(),
+    delete_photos: z.array(z.string()).optional(),
   });
 
   type updateFormSchemaType = z.infer<typeof updateformSchema>;
@@ -88,28 +87,33 @@ const EditDiscussionModal = () => {
     defaultValues: {
       description: "",
       departments: [],
-      photosToCreate: [],
-      photosToDelete: [],
+      new_photos: [],
+      delete_photos: [],
     },
     mode: "all",
   });
 
-  const isLoading = form.formState.isSubmitting;
-
   const setInitialPhotos = () => {
-    if(data.post && data.post?.photos.length > 0) {
-        setFilesToDisPlay(data?.post?.photos.map((photo) => ({url: photo.public_url, id: photo.public_id})) || [])
+    if (data.post && data.post?.photos.length > 0) {
+      setFilesToDisPlay(
+        data?.post?.photos.map((photo) => ({
+          url: photo.public_url,
+          id: photo.public_id,
+        })) || []
+      );
     }
-  }
+  };
 
   useEffect(() => {
-    if(data.post && data.post?.department.length > 0) {
-        form.setValue('departments', [...data?.post?.department?.map(d => d.id)])
+    if (data.post && data.post?.department.length > 0) {
+      form.setValue("departments", [
+        ...data?.post?.department?.map((d) => d.id),
+      ]);
     } else {
-        form.setValue('departments', [session?.user?.departmentId as string])
+      form.setValue("departments", [session?.user?.departmentId as string]);
     }
-    form.setValue('description', data?.post?.description as string)
-    setInitialPhotos()
+    form.setValue("description", data?.post?.description as string);
+    setInitialPhotos();
     return () => {
       form.reset();
     };
@@ -148,26 +152,108 @@ const EditDiscussionModal = () => {
     value: department.id,
   }));
 
-  const currentDepartment = departments.data?.find((department) => department.id == session?.user.departmentId)
-
-  // upload photo
+  const currentDepartment = departments.data?.find(
+    (department) => department.id == session?.user.departmentId
+  );
 
   // remove file
-  const removeFiles = (file: { url: string; id: number | string }) => {
+  const removeFiles = (file: { url: string; id: string }) => {
+    const newPhotos = form.getValues("new_photos") as {
+      id: string;
+      file: File;
+    }[];
+    const deletePhotos = form.getValues("delete_photos") as string[];
 
+    const filteredNewPhotos = newPhotos?.filter((photo) => photo.id != file.id);
+    setFilesToDisPlay((prev) =>
+      prev.filter((fileToDisplay) => fileToDisplay.id != file.id)
+    );
+
+    form.setValue("new_photos", filteredNewPhotos);
+    form.setValue("delete_photos", [...deletePhotos, file.id]);
   };
   const { toast } = useToast();
 
+  const updateDiscussion = useMutateProcessor<
+    UpdatePostSchemaType,
+    PostSchemaType
+  >(`/posts/${data.post?.id}`, null, "PATCH", ["discussions"]);
+
+  const isLoading =
+    form.formState.isSubmitting || updateDiscussion.status === "pending";
+
   const onSubmit: SubmitHandler<updateFormSchemaType> = async (values) => {
     // we append all the file into files array to make it iterateable
-    
-    console.log(values)
-    console.log(filesToDisplay)
+    const { departments, description } = values;
+    const deletePhotos = values.delete_photos?.filter((deletePhoto) =>
+      deletePhoto.includes("next-alumni-system")
+    );
+
+    if (values.new_photos && values.new_photos?.length > 0) {
+      const files = [];
+      for (const file of values.new_photos) {
+        files.push(file);
+      }
+      const newPhotos = await Promise.all(
+        files.map((data: { file: File; id: number | string }) =>
+          uploadPhotoForum(data)
+        )
+      );
+      updateDiscussion.mutate(
+        {
+          new_photos: newPhotos,
+          delete_photos: deletePhotos,
+          department: departments,
+          description,
+        },
+        {
+          onSuccess(data, variables, context) {
+            toast({
+              variant: "default",
+              description: "Post updated.",
+            });
+            onHandleClose();
+          },
+          onError(error, variables, context) {
+            console.log(error)
+            toast({
+              variant: "destructive",
+              description: "Something went wrong...",
+            });
+          },
+        }
+      );
+    } else {
+      updateDiscussion.mutate(
+        { delete_photos: deletePhotos, department: departments, description },
+        {
+          onSuccess(data, variables, context) {
+            toast({
+              variant: "default",
+              description: "Post updated.",
+            });
+            onHandleClose();
+          },
+          onError(error, variables, context) {
+            console.log(error)
+            toast({
+              variant: "destructive",
+              description: "Something went wrong...",
+            });
+          },
+        }
+      );
+    }
   };
 
-  const allowedRoles = ['ADMIN', 'COORDINATOR', 'BULSU_PARTNER', 'PESO', 'ADVISER']
+  const allowedRoles = [
+    "ADMIN",
+    "COORDINATOR",
+    "BULSU_PARTNER",
+    "PESO",
+    "ADVISER",
+  ];
 
-  console.log(data?.post)
   return (
     <div>
       <Dialog open={isModalOpen} onOpenChange={onHandleClose}>
@@ -194,15 +280,17 @@ const EditDiscussionModal = () => {
                     name="departments"
                     render={() => (
                       <FormItem>
-                        {
-                           allowedRoles.includes(session?.user?.role as string) ? 
-                           (<Popover open={open} onOpenChange={setOpen}>
-                            <PopoverTrigger asChild>
+                        {allowedRoles.includes(
+                          session?.user?.role as string
+                        ) ? (
+                          <Popover open={open} onOpenChange={setOpen}>
+                            <PopoverTrigger asChild disabled={isLoading}>
                               <Button
                                 variant="link"
                                 role="combobox"
                                 aria-expanded={open}
                                 className="justify-between text-zinc-500 p-0 capitalize"
+                                disabled={isLoading}
                               >
                                 Select department
                                 <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
@@ -211,7 +299,9 @@ const EditDiscussionModal = () => {
                             <PopoverContent className="max-w-[200px] p-0">
                               <Command>
                                 <CommandInput placeholder="Search departments." />
-                                <CommandEmpty>No department found.</CommandEmpty>
+                                <CommandEmpty>
+                                  No department found.
+                                </CommandEmpty>
                                 <CommandGroup className="">
                                   {selectableDepartments?.map((item) => (
                                     <CommandItem key={item.value}>
@@ -230,7 +320,9 @@ const EditDiscussionModal = () => {
                                                   checked={field.value?.includes(
                                                     item.value
                                                   )}
-                                                  onCheckedChange={(checked) => {
+                                                  onCheckedChange={(
+                                                    checked
+                                                  ) => {
                                                     return checked
                                                       ? field.onChange([
                                                           ...field.value,
@@ -239,7 +331,8 @@ const EditDiscussionModal = () => {
                                                       : field.onChange(
                                                           field.value?.filter(
                                                             (value) =>
-                                                              value !== item.value
+                                                              value !==
+                                                              item.value
                                                           )
                                                         );
                                                   }}
@@ -257,10 +350,13 @@ const EditDiscussionModal = () => {
                                 </CommandGroup>
                               </Command>
                             </PopoverContent>
-                          </Popover> )
-                          : <span className="justify-between text-zinc-500 p-0 capitalize">{currentDepartment?.name.toLowerCase()}</span>
-                        }
-                         
+                          </Popover>
+                        ) : (
+                          <span className="justify-between text-zinc-500 p-0 capitalize">
+                            {currentDepartment?.name.toLowerCase()}
+                          </span>
+                        )}
+
                         <FormMessage />
                       </FormItem>
                     )}
@@ -291,24 +387,24 @@ const EditDiscussionModal = () => {
                   )}
                 />
                 <div className="flex flex-col max-h-[15em] overflow-y-auto gap-y-2 mt-5">
-                    {filesToDisplay.map((file) => {
-                        return (
-                          <div
-                            key={file.id}
-                            className="relative rounded-md w-full max-h-[20em]"
-                          >
-                            <X
-                              className="w-5 h-5 absolute top-2 rounded-md right-2 z-10 cursor-pointer bg-white text-zinc-600"
-                              onClick={() => removeFiles(file)}
-                            />
-                            <img
-                              src={file.url}
-                              alt="post image"
-                              className="h-full w-full object-cover"
-                            />
-                          </div>
-                        );
-                      })}
+                  {filesToDisplay.map((file) => {
+                    return (
+                      <div
+                        key={file.id}
+                        className="relative rounded-md w-full max-h-[20em]"
+                      >
+                        <X
+                          className="w-5 h-5 absolute top-2 rounded-md right-2 z-10 cursor-pointer bg-white text-zinc-600"
+                          onClick={() => removeFiles(file)}
+                        />
+                        <img
+                          src={file.url}
+                          alt="post image"
+                          className="h-full w-full object-cover"
+                        />
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
 
@@ -318,29 +414,31 @@ const EditDiscussionModal = () => {
                   <ActionTooltip label="Photo/video">
                     <FormField
                       control={form.control}
-                      name="photosToCreate"
+                      name="new_photos"
                       render={({ field }) => (
                         <FormItem className="w-full">
                           <label htmlFor="photos">
-                            <ImageUpload
-                              className="w-7 h-7 cursor-pointer text-zinc-500 dark:text-white hover:text-zinc-600 dark:hover:text-zinc-300"
-                            />
+                            <ImageUpload className="w-7 h-7 cursor-pointer text-zinc-500 dark:text-white hover:text-zinc-600 dark:hover:text-zinc-300" />
                           </label>
                           <FormControl>
                             <Input
-                              {...form.register("photosToCreate")}
+                              {...form.register("new_photos")}
                               className="hidden"
                               id="photos"
                               type="file"
                               accept="image/*"
                               multiple
                               onChange={async (e) => {
-                                setInitialPhotos() // initiate a photo to display array
+                                setInitialPhotos(); // initiate a photo to display array
                                 const files = e.target.files;
 
                                 if (files && files?.length > 0) {
                                   const filesToDisplay = [];
-                                  for (let index = 0;index < files.length;index++ ) {
+                                  for (
+                                    let index = 0;
+                                    index < files.length;
+                                    index++
+                                  ) {
                                     filesToDisplay.push({
                                       file: files[index],
                                       id: createId(),
@@ -352,7 +450,10 @@ const EditDiscussionModal = () => {
                                   );
 
                                   // convertToBase64 Function
-                                  const convertToBase64 = (data: {file: File; id: number | string;}) => {
+                                  const convertToBase64 = (data: {
+                                    file: File;
+                                    id: string;
+                                  }) => {
                                     const reader = new FileReader();
                                     reader.readAsDataURL(data.file);
                                     reader.onloadend = () => {
@@ -369,14 +470,11 @@ const EditDiscussionModal = () => {
                                   // converting all the files to 64
                                   await Promise.all(
                                     filesToDisplay.map(
-                                      (data: {
-                                        file: File;
-                                        id: number | string;
-                                      }) => convertToBase64(data)
+                                      (data: { file: File; id: string }) =>
+                                        convertToBase64(data)
                                     )
                                   );
                                 }
-
                               }}
                             />
                           </FormControl>
@@ -411,7 +509,7 @@ const EditDiscussionModal = () => {
                 >
                   {(() => {
                     if (isLoading) return <Loader size={25} />;
-                    return "Post";
+                    return "Update";
                   })()}
                 </Button>
               </DialogFooter>
--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -8,8 +8,10 @@ cloudinary.config({
 
 export const cloudinaryDestroy = async (public_id: string) => {
   try {
+
+    const key = public_id.includes('next-alumni-system') ? public_id : `next-alumni-system/${public_id}`
     const result = await cloudinary.uploader.destroy(
-      `next-alumni-system/${public_id}`
+      key
     );
 
     console.log(result);
